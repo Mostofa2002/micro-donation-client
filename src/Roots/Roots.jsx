5a2122db0eed9d3daf3b770ad2787a6a327c0f09
Mostofa2002 <taj.2020x@gmail.com>
2023-11-05 11:37:08 +0600

    footer added

--- src/Roots/Roots.jsx
+++ src/Roots/Roots.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../pages/Nav";
+import Footer from "../pages/Footer";
 
 const Roots = () => {
   return (
     <div>
       <Nav></Nav>
       <Outlet></Outlet>
+      <Footer />
     </div>
   );
 };
